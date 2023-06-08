20112d1ce900340a57ef725ba2997e9ff14d99ed
udesurd <udesurd@gmail.com>
2023-06-08 16:54:16 +0300

    [upd] add views, urls

--- inova_app/views.py
+++ inova_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'inova_app/registerPage.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'inova_app/loginPage.html')
+
+
+def homePage(request):
+    return render(request, "inova_app/homePage.html")
+
---
+++ inova_crm/README.txt
@@ -0,0 +1,9 @@
+1. Create project
+2. Create app (add app to settings)
+3. Create models
+4. Do migration
+5. Create views
+6. Create url's
+7. Create app/templates/app
+8. Create html
+9 Render html in views
